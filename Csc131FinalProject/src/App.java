17c30bdded2630ca311ff89bf4093149d32a1e17
Nexus <99375177+NexusOmegaRay@users.noreply.github.com>
2022-04-27 17:37:58 -0700

    Update App.java

--- Csc131FinalProject/src/App.java
+++ Csc131FinalProject/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World! This file should be deleted!");
+        System.out.println("Hello, World! This file should be deleted! I agree!");
     }
 }
